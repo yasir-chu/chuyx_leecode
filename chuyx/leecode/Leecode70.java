c4d647e15b733aa73140aa4f9cbfc824c0cbab34
yuxiang.chu <yuxiang.chu@corp.to8to.com>
2021-09-28 21:59:03 +0800

    leecode70爬楼梯

---
+++ chuyx/leecode/Leecode70.java
@@ -0,0 +1,32 @@
+package leecode;
+
+/**
+ * @author yuxiang.chu
+ */
+public class Leecode70{
+    public static void main(String[] args) {
+        Solution solution = new Leecode70().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int climbStairs(int n) {
+            int a = 1, b=0, c=0;
+            for (int i = 1; i <= n; i++) {
+                c=0;
+                if (i == 1){
+                    c = a;
+                }
+                if (i > 1){
+                    c = b + a;
+                }
+                b=a;
+                a=c;
+            }
+            return c;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
