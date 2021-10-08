bdf05ad168362a3772e6091a5075520b6add4973
yuxiang.chu <yuxiang.chu@corp.to8to.com>
2021-10-08 18:23:29 +0800

    修改gitignore

--- chuyx/leecode/LeetCode70.java
+++ chuyx/leecode/LeetCode70.java
@@ -1,5 +1,28 @@
 package leecode;
-
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+//
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+//
+// 注意：给定 n 是一个正整数。
+//
+// 示例 1：
+//
+// 输入： 2
+//输出： 2
+//解释： 有两种方法可以爬到楼顶。
+//1.  1 阶 + 1 阶
+//2.  2 阶
+//
+// 示例 2：
+//
+// 输入： 3
+//输出： 3
+//解释： 有三种方法可以爬到楼顶。
+//1.  1 阶 + 1 阶 + 1 阶
+//2.  1 阶 + 2 阶
+//3.  2 阶 + 1 阶
+//
+// Related Topics 记忆化搜索 数学 动态规划 👍 1903 👎 0
 /**
  * @author yuxiang.chu
  */
@@ -9,6 +32,9 @@ public class LeetCode70 {
 
     }
 
+    /**
+     * 递归算法就是将大问题拆分成小问题 用小问题的答案整合成大问题的答案
+     */
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int climbStairs(int n) {
