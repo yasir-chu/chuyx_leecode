dd77d3763530f11362c5f3c954eb1a29e67ecb92
yuxiang.chu <yuxiang.chu@corp.to8to.com>
2022-01-25 15:05:24 +0800

    leeCode83

--- chuyx/leecode/LeetCode83.java
+++ chuyx/leecode/LeetCode83.java
@@ -5,4 +5,27 @@ package leecode;
  * @date 2022/1/25 9:23
  **/
 public class LeetCode83 {
+
+    public static void main(String[] args) {
+        Solution solution = new LeetCode83().new Solution();
+
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode item = head;
+            while (item.next != null){
+                if (item.val == item.next.val){
+                    item.next = item.next.next;
+                    continue;
+                }
+                item = item.next;
+            }
+            return head;
+        }
+    }
 }
