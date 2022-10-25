3edae5552809b4ab76ec78ef60288c58debb2721
yuxiang_chu <yuxiang_chu@feidee.com>
2022-10-25 09:58:29 +0800

    leetcode 915

---
+++ chuyx/leecode/LeetCode915.java
@@ -0,0 +1,54 @@
+package leecode;
+
+/**
+ * @author yuxiang_chu
+ * @date 2022/10/24 14:40
+ */
+public class LeetCode915 {
+
+    public static void main(String[] args) {
+        Solution solution = new LeetCode915().new Solution();
+        int[] a= {5,0,3,8,6};
+        System.out.println(solution.partitionDisjoint(a));
+    }
+
+    class Solution {
+        public int partitionDisjoint(int[] nums) {
+            int result = 0;
+            int leftMax = nums[0];
+            int nowMax = nums[0];
+            int rightMin = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                if (nowMax < nums[i]) {
+                    nowMax = nums[i];
+                }
+                if (rightMin > nums[i]) {
+                    rightMin = nums[i];
+                }
+                if (leftMax > rightMin) {
+                    rightMin  = Integer.MAX_VALUE;
+                    result = i;
+                    leftMax = nowMax;
+                }
+
+            }
+            return result + 1;
+        }
+
+
+        public int partitionDisjoint2(int[] nums) {
+            int result = 0,leftMax = nums[0],max = leftMax;
+            for (int i = 1; i < nums.length; i++) {
+                if (leftMax < nums[i]) {
+                    leftMax = max;
+                    result = i;
+                }else{
+                    max = Math.max(max, nums[i]);
+                }
+            }
+            return result + 1;
+        }
+    }
+}
+
+
