a5de598d54388865928c92d2b9808df9316a7ea7
yuxiang_chu <yuxiang_chu@feidee.com>
2022-11-02 16:33:59 +0800

    leetCode481 & testSomeThing

---
+++ chuyx/test/TestIntegerPar.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author yuxiang_chu
+ * @date 2022/10/31 14:11
+ */
+public class TestIntegerPar {
+    public static void main(String[] args) {
+        String a = "00";
+        System.out.println(Integer.parseInt(a));
+    }
+}
---
+++ chuyx/test/threadpool/TestMapRemove.java
@@ -0,0 +1,19 @@
+package test.threadpool;
+
+import java.util.HashMap;
+
+/**
+ * @author yuxiang_chu
+ * @date 2022/11/1 10:03
+ */
+public class TestMapRemove {
+
+    public static void main(String[] args) {
+        StringBuilder a = new StringBuilder();
+        a.append("123123").append(",");
+
+        System.out.println(a.substring(0, a.length() - 1).toString());
+
+
+    }
+}
